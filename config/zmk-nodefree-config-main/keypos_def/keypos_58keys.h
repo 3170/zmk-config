e172b62565e10c956f9752181d11ec6626b94e77
3170 <mail@eilertjanssen.com>
2022-11-02 10:51:29 +0100

    Edited Nodefree matrix, included in keymap, edited keymap

---
+++ config/zmk-nodefree-config-main/keypos_def/keypos_58keys.h
@@ -0,0 +1,81 @@
+/*                                      58 KEY MATRIX / LAYOUT MAPPING
+
+  ╭─────────────────────────╮       ╭────────────────────────╮  ╭─────────────────────────╮       ╭─────────────────────────╮
+  │  0   1   2   3   4   5  │       │  6   7   8   9  10  11 │  │ LT5 LT4 LT3 LT2 LT1 LT0 │       │ RT0 RT1 RT2 RT3 RT4 RT5 │
+  │ 12  13  14  15  16  17  │       │ 18  19  20  21  22  23 │  │ LU5 LU4 LU3 LU2 LU1 LU0 │       │ RU0 RU1 RU2 RU3 RU4 RU5 │
+  │ 24  25  26  27  28  29  │       │ 30  31  32  33  34  35 │  │ LL5 LL4 LL3 LL2 LL1 LL0 │       │ RL0 RL1 RL2 RL3 RL4 RL5 │
+  │ 36  37  38  39  40  41  ╰───┬───╯ 42  43  44  45  46  47 │  │ LB5 LB4 LB3 LB2 LB1 LB0 ╰───┬───╯ RB0 RB1 RB2 RB3 RB4 RB5 │
+  ╰───────╮ 48  49  50  51  52  │ 53  54  55  56  57 ╭───────╯  ╰───────╮ LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 ╭───────╯
+          ╰─────────────────────┴────────────────────╯                  ╰─────────────────────┴─────────────────────╯
+ */
+
+#pragma once
+
+#define LT0  5  // left-top row
+#define LT1  4
+#define LT2  3
+#define LT3  2
+#define LT4  1
+#define LT5  0
+
+#define RT0  6  // right-top row
+#define RT1  7
+#define RT2  8
+#define RT3  9
+#define RT4 10
+#define RT5 11
+
+#define LU0 17  // left-upper row
+#define LU1 16
+#define LU2 15
+#define LU3 14
+#define LU4 13
+#define LU5 12
+
+#define RM0 18  // right-upper row
+#define RM1 19
+#define RM2 20
+#define RM3 21
+#define RM4 22
+#define RM5 23
+
+#define LL0 29  // left-lower row
+#define LL1 28
+#define LL2 27
+#define LL3 26
+#define LL4 25
+#define LL5 24
+
+#define RL0 30  // right-lower row
+#define RL1 31
+#define RL2 32
+#define RL3 33
+#define RL4 34
+#define RL5 35
+
+#define LB0 41  // left-bottom row
+#define LB1 40
+#define LB2 39
+#define LB3 38
+#define LB4 37
+#define LB5 36
+
+#define RB0 42  // right-bottom row
+#define RB1 43
+#define RB2 44
+#define RB3 45
+#define RB4 46
+#define RB5 47
+
+#define LH0 52  // left thumb keys
+#define LH1 51
+#define LH2 50
+#define LH3 49
+#define LH4 48
+
+#define RH0 53  // right thumb keys
+#define RH1 54
+#define RH2 55
+#define RH3 56
+#define RH4 57
+
