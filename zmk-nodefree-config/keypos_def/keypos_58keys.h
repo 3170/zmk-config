fb5e624faef7a080becf9e81f1e7558d92fc4468
3170 <mail@eilertjanssen.com>
2022-11-10 11:27:07 +0100

    Added Keymap config File to edit Parameters in helper.h / Updated documentation

--- zmk-nodefree-config/keypos_def/keypos_58keys.h
+++ zmk-nodefree-config/keypos_def/keypos_58keys.h
@@ -3,9 +3,9 @@
   ╭─────────────────────────╮       ╭────────────────────────╮  ╭─────────────────────────╮       ╭─────────────────────────╮
   │  0   1   2   3   4   5  │       │  6   7   8   9  10  11 │  │ LT5 LT4 LT3 LT2 LT1 LT0 │       │ RT0 RT1 RT2 RT3 RT4 RT5 │
   │ 12  13  14  15  16  17  │       │ 18  19  20  21  22  23 │  │ LU5 LU4 LU3 LU2 LU1 LU0 │       │ RU0 RU1 RU2 RU3 RU4 RU5 │
-  │ 24  25  26  27  28  29  │       │ 30  31  32  33  34  35 │  │ LL5 LL4 LL3 LL2 LL1 LL0 │       │ RL0 RL1 RL2 RL3 RL4 RL5 │
-  │ 36  37  38  39  40  41  ╰───┬───╯ 42  43  44  45  46  47 │  │ LB5 LB4 LB3 LB2 LB1 LB0 ╰───┬───╯ RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰───────╮ 48  49  50  51  52  │ 53  54  55  56  57 ╭───────╯  ╰───────╮ LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 ╭───────╯
+  │ 24  25  26  27  28  29  ╰───┬───╯ 30  31  32  33  34  35 │  │ LL5 LL4 LL3 LL2 LL1 LL0 ╰───┬───╯ RL0 RL1 RL2 RL3 RL4 RL5 │
+  │ 36  37  38  39  40  41  42  │ 43  44  45  46  47  48  49 │  │ LB6 LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 RB6 │
+  ╰───────╮ 50  51  52  53      │     54  55  56  57 ╭───────╯  ╰───────╮ LH3 LH2 LH1 LH0     │     RH0 RH1 RH2 RH3 ╭───────╯
           ╰─────────────────────┴────────────────────╯                  ╰─────────────────────┴─────────────────────╯
  */
 
@@ -53,29 +53,30 @@
 #define RL4 34
 #define RL5 35
 
-#define LB0 41  // left-bottom row
-#define LB1 40
-#define LB2 39
-#define LB3 38
-#define LB4 37
-#define LB5 36
+#define LB0 42  // left-bottom row
+#define LB1 41
+#define LB2 40
+#define LB3 39
+#define LB4 38
+#define LB5 37
+#define LB6 36
 
-#define RB0 42  // right-bottom row
-#define RB1 43
-#define RB2 44
-#define RB3 45
-#define RB4 46
-#define RB5 47
+#define RB0 43  // right-bottom row
+#define RB1 44
+#define RB2 45
+#define RB3 46
+#define RB4 47
+#define RB5 48
+#define RB6 49
 
-#define LH0 52  // left thumb keys
-#define LH1 51
-#define LH2 50
-#define LH3 49
-#define LH4 48
+#define LH0 53  // left thumb keys
+#define LH1 52
+#define LH2 51
+#define LH3 50
 
-#define RH0 53  // right thumb keys
-#define RH1 54
-#define RH2 55
-#define RH3 56
-#define RH4 57
+#define RH0 54  // right thumb keys
+#define RH1 55
+#define RH2 56
+#define RH3 57
+
 
